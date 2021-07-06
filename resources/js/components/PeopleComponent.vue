06de2a805d3834ca034d50d32d0c843e7258ea41
chuksdsilent <chuksdsilent@gmail.com>
2021-07-06 02:58:58 +0100

    first commit

---
+++ resources/js/components/PeopleComponent.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="container">
+    <div class=" justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">People</div>
+
+                <div class="card-body">
+                  
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+    export default {
+         data(){
+           return{
+               people:[]
+           }
+        },
+        created(){
+            this.fetchPeople();
+        },
+        methods: {
+            fetchPeople(){
+                fetch('https://swapi.dev/api/people')
+                .then(res => res.json())
+                .then(res =>{
+                    console.log(res);
+                })
+            }
+        }
+
+    }
+</script>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<div id="app">
+    <people-component></people-component>
+</div>
+@endsection
